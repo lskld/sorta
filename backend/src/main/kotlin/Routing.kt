c70784cecf06f7bcf39b4e818acc177addbc76df
lskld <theo.lillieskold@gmail.com>
2026-07-29 12:14:23 +0200

    add local variables

--- backend/src/main/kotlin/Routing.kt
+++ backend/src/main/kotlin/Routing.kt
@@ -1,10 +1,34 @@
 package com.lskld
 
+import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer
+import ai.onnxruntime.OrtEnvironment
+import ai.onnxruntime.OrtSession
+import com.pgvector.PGvector
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import query.getReferenceDate
+import java.nio.file.Paths
+import java.sql.DriverManager
 
 fun Application.configureRouting() {
+
+    val env = OrtEnvironment.getEnvironment()
+    val session = env.createSession(
+        "models/bge-small-en-v1.5-onnx/model.onnx",
+        OrtSession.SessionOptions()
+    )
+    val tokenizer = HuggingFaceTokenizer.newInstance(Paths.get("models/bge-small-en-v1.5-onnx"))
+
+    val connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5433/sorta",
+        "postgres",
+        "postgres"
+    )
+    PGvector.addVectorType(connection)
+
+    val refDate = getReferenceDate(connection)
+
     routing {
         get("/") {
             call.respondText("Hello, World!")
