2343ef5cb89be9dfbe2f7c539c4cb511ba9bb2aa
lskld <theo.lillieskold@gmail.com>
2026-08-07 00:22:22 +0200

    made cards pressable/selectable and highlighting them for future anchor possibilities when querying

--- client/components/ProductCard.tsx
+++ client/components/ProductCard.tsx
@@ -1,16 +1,22 @@
-import { View, Text, StyleSheet } from "react-native"
-import { Product } from "../data/mockProducts"
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Product } from "../data/mockProducts";
 
-export default function ProductCard({ product }: {
-    product: Product
-}) {
-    return (
-        <View style={styles.card}>
-                <Text style={styles.text}>{product.name}</Text>
-                <Text style={styles.text}>{product.category}</Text>
-                <Text style={styles.text}>Units sold: {product.units_sold}</Text>
-        </View>
-    )
+type Props = {
+	product: Product;
+	isSelected: boolean;
+	onPress: () => void;
+};
+
+export default function ProductCard({ product, isSelected, onPress }: Props) {
+	return (
+		<Pressable onPress={onPress}>
+			<View style={[styles.card, isSelected && styles.selectedCard]}>
+				<Text style={styles.text}>{product.name}</Text>
+				<Text style={styles.text}>{product.category}</Text>
+				<Text style={styles.text}>Units sold: {product.units_sold}</Text>
+			</View>
+		</Pressable>
+	);
 }
 
 const styles = StyleSheet.create({
@@ -30,6 +36,10 @@ const styles = StyleSheet.create({
 		shadowRadius: 2,
 		// Shadow for Android
 		elevation: 3,
+	},
+	selectedCard: {
+		borderColor: "#007aff",
+		borderWidth: 2,
 	},
 	text: {
 		textAlign: "center",
--- client/components/ProductList.tsx
+++ client/components/ProductList.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { mockProducts } from "../data/mockProducts";
 import { ScrollView } from "react-native";
 import ProductCard from "./ProductCard";
 
 export default function ProductList() {
+	const [selectedId, setSelectedId] = useState<number | null>(null);
+
 	return (
 		<ScrollView style={{ width: "100%" }}>
 			{mockProducts.map((product) => (
-				<ProductCard product={product} key={product.product_id} />
+				<ProductCard
+					product={product}
+					key={product.product_id}
+					isSelected={selectedId === product.product_id}
+					onPress={() => setSelectedId(product.product_id)}
+				/>
 			))}
 		</ScrollView>
 	);
